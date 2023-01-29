6296ca6e98d1752168450b4cd2e14564e4b3e7d6
Artem Fedulov <arfedulov@gmail.com>
2023-01-29 22:32:12 +0300

    add on submit validation

--- js-forms/web/index.js
+++ js-forms/web/index.js
@@ -21,35 +21,40 @@ const formIds = [
 formIds.forEach((id) => {
   const form = $(id);
   form.addEventListener("submit", onSubmit);
-  form.addEventListener("blur", validateOnBlur, true);
+  form.addEventListener("blur", (e) => validateOnBlur(e.target), true);
 });
 
 function onSubmit(event) {
   event.preventDefault();
 
+  const isValid = customValidateOnSubmit(event);
+  if (!isValid) {
+    return;
+  }
+
   const formData = new FormData(event.target);
   console.log(Object.fromEntries(formData.entries()));
 }
 
-function updateCustomValidity(event) {
-  const rule = rules[event.target.name];
+function updateCustomValidity(inputElement) {
+  const rule = rules[inputElement.name];
   if (!rule) {
     return;
   }
 
-  const formData = new FormData(event.target.form);
-  const validity = rule(formData, event.target.value);
-  event.target.setCustomValidity(validity);
+  const formData = new FormData(inputElement.form);
+  const validity = rule(formData, inputElement.value);
+  inputElement.setCustomValidity(validity);
 }
 
-function validateOnBlur(event) {
-  const isValid = event.target.validity.valid;
-  const descriptionElement = $(event.target.getAttribute("aria-describedby"));
+function validateOnBlur(inputElement) {
+  updateCustomValidity(inputElement);
 
-  updateCustomValidity(event);
+  const isValid = inputElement.validity.valid;
+  const descriptionElement = $(inputElement.getAttribute("aria-describedby"));
 
   if (!isValid) {
-    const message = event.target.validationMessage;
+    const message = inputElement.validationMessage;
 
     if (!descriptionElement.dataset.infoDescription) {
       // memorize info text
@@ -59,11 +64,27 @@ function validateOnBlur(event) {
     descriptionElement.innerText = message;
     descriptionElement.classList.add("form__input-description--invalid");
   } else {
-    if (descriptionElement.dataset.infoDescription) {
+    if (descriptionElement && descriptionElement.dataset.infoDescription) {
       // restore info description
       descriptionElement.innerText = descriptionElement.dataset.infoDescription;
     }
 
-    descriptionElement.classList.remove("form__input-description--invalid");
+    descriptionElement?.classList.remove("form__input-description--invalid");
   }
 }
+
+function customValidateOnSubmit(event) {
+  let isValid = true;
+
+  for (const controll of Array.from(event.target)) {
+    validateOnBlur(controll);
+    const valid = event.target.reportValidity();
+    if (!valid) {
+      isValid = false;
+    }
+  }
+
+  return isValid;
+}
+
+// TODO: add on-submit validation
